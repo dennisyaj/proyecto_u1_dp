2f96b0933e347dd45fa8d92bbc09e7c6a2cd778a
Dennis Pilco <dennispilco@gmail.com>
2022-12-08 19:48:27 -0500

    taller 10

--- js/funciones.js
+++ js/funciones.js
@@ -5,8 +5,19 @@ function multiplicar(num1, num2) {
     return num1 * num2;
 }
 function sumar(num1, num2) {
-    return  num1 + num2;
+    return num1 + num2;
 }
 function restar(num1, num2) {
     return num1 - num2;
+}
+
+function encender() {
+    var source = document.getElementById('foco').src
+    if (source.includes('1.png')) {
+        return '../img/2.png'
+    }
+    if (source.includes('2.png')) {
+        return '../img/1.png'
+    }
+
 }
